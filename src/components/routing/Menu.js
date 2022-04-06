44716ac898a9bed1e919e4d01cac02ced0b44a25
shivam-isystango <sgour@isystango.com>
2022-04-06 19:19:12 +0530

    worked on routing

--- src/components/routing/Menu.js
+++ src/components/routing/Menu.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-        <Link to="/">Home</Link><br /><br />
-        <Link to="/about">About Us</Link><br /><br />
-        <Link to="/contact">Contact Us</Link><br /><br />
-        <Link to="/user/shivam/gour">User</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link to="/" className="navbar-brand">Navbar</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/about" className="nav-link">About Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/contact" className="nav-link">Contact Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/user/shivam/gour" className="nav-link">User</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
